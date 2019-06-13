ee571652d2a2cceaacc98ac46762faf153e28930
Giovanni Ravalico <ravalico.giovanni@gmail.com>
2019-06-13 15:49:38 +0200

    refactor(ShipmentsView): adopted ViewContainer

--- src/views/shipments-view/ShipmentsView.tsx
+++ src/views/shipments-view/ShipmentsView.tsx
@@ -1,26 +1,9 @@
 import * as React from 'react'
 import { RouteChildrenProps } from 'react-router'
 import { useSelector, useDispatch, shallowEqual } from 'react-redux'
-import styled from '@emotion/styled/macro'
 
-import Typography from '@material-ui/core/Typography'
-
-import Container from '@material-ui/core/Container'
-
-import { SearchBar, ShipmentsListContainer } from 'components'
+import { SearchBar, ShipmentsListContainer, ViewContainer } from 'components'
 import { shipmentsSelectors, shipmentsActions } from 'features/shipments'
-
-const ViewContainer = styled.div`
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  justify-content: flex-start;
-
-  width: 100%;
-  padding-top: 30px;
-
-  text-align: center;
-`
 
 export const ShipmentsView: React.FC<RouteChildrenProps> = ({ history }) => {
   const dispatch = useDispatch()
@@ -40,19 +23,14 @@ export const ShipmentsView: React.FC<RouteChildrenProps> = ({ history }) => {
   }, [fetchShipments])
 
   return (
-    <ViewContainer>
-      <Typography variant="h4" component="h2">
-        Shipments
-      </Typography>
-      <Container maxWidth={'md'}>
-        <SearchBar />
-        <ShipmentsListContainer
-          shipments={shipments}
-          onSelectedShipment={shipmentId =>
-            history.push(`/shipment/${shipmentId}`)
-          }
-        />
-      </Container>
+    <ViewContainer title={'Shipments'}>
+      <SearchBar />
+      <ShipmentsListContainer
+        shipments={shipments}
+        onSelectedShipment={shipmentId =>
+          history.push(`/shipment/${shipmentId}`)
+        }
+      />
     </ViewContainer>
   )
 }
